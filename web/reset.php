5c1aab1340a375d02cdaf098e96f6ee618f5fc4d
ff <ff@lomnido.com>
2015-04-24 16:58:17 +0200

    change default settings

--- web/reset.php
+++ web/reset.php
@@ -5,8 +5,6 @@ include("inc/functions.php");
 $jsonSettings=loadSettings();
 $settings=json_decode($jsonSettings);
 
-exec(RESET_COMMAND, $output, $ret);
-
 $settingsResetText=file_get_contents("reset/settings.json");
 $settingsResetJSON=json_decode($settingsResetText);
 # IP Adressen nicht resetten!
